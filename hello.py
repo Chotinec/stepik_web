0d1fd9aca8f4cb76ca71fab993f68c435f337871
Igor Merkuryev <imerkuryev@luxoft.com>
2018-06-20 06:45:50 +0300

    fix3

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def application(environ, start_response):
         status='200 OK'
         headers=[
-                ('Content-Type', 'text/plain')
+                ('Content-type','text/plain')
         ]
         satrt_response(status, headers)
-        return [bytes(i+'\n', 'ascii')for i in environ['QUERY_STRING'].split('&')]
+        return [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
