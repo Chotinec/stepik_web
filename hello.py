c384cb6fcf7ebbffc5a4d12325647b0add97a724
Igor Merkuryev <imerkuryev@luxoft.com>
2018-06-24 06:51:00 +0300

    fix11

--- hello.py
+++ hello.py
@@ -1,13 +1,17 @@
-def app(environ, start_response):
-    # Returns a dictionary in which the values are lists
+from cgi import parse_qs
 
-    data = environ['QUERY_STRING']
-    d = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+def application(environ, start_response):
 
-    status = '200 OK'
-    response_headers = [
-        ('Content-type','text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
-    start_response(status, response_headers)
-    return d
+  query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+  body = []
+  for key, values in query.items():
+    for item in values:
+      body.append(key + "=" + item + "\r\n")
+   
+  status = '200 OK'
+  headers = [
+   ('Content-Type', 'text/plain')
+  ]
+  
+  start_response(status, headers)
+  return body
