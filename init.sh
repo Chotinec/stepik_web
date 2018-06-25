5ac6f39da738881741b87d521c7e50065a2aad8c
Igor Merkuryev <imerkuryev@luxoft.com>
2018-06-25 05:58:12 +0300

    Django app

--- init.sh
+++ init.sh
@@ -2,5 +2,7 @@
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/default
+
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/default
+sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
