2c3168ef1b44c04f1e3372e98f6a4710a36dbd80
Igor Merkuryev <imerkuryev@luxoft.com>
2018-06-26 01:49:32 +0300

    Django_app_fix7

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from qa.views import test
 
-urlpatterns = patterns('qa.views',                                              
-   url(r'^$', 'test'),                                                              
-   url(r'^login/.*$', 'test', name='login'),                                    
-   url(r'^signup/.*', 'test', name='signup'),                                   
-   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),                 
-   url(r'^ask/.*', 'test', name='ask'),                                         
-   url(r'^popular/.*', 'test', name='popular'),                                 
-   url(r'^new/.*', 'test', name='new'),                                         
-)    
+urlpatterns = [                                              
+   url(r'^$', test, name='test'),                                                              
+   url(r'^login/.*$', test, name='test'),                                    
+   url(r'^signup/.*', test, name='test'),                                   
+   url(r'^question/(?P<id>\d+)/$', test, name='test'),                 
+   url(r'^ask/.*', 'test', name='test'),                                         
+   url(r'^popular/.*', test, name='test'),                                 
+   url(r'^new/.*', test, name='test'),                                         
+]    
 
 
--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/default
+#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/default
 sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
